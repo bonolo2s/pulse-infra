ef990de73a3214e09c6292fcecfb74e4774432fd
Bonolo Selowa <bonololloyd003@gmail.com>
2026-05-05 15:24:01 +0200

    lambda added

---
+++ lib/lambda-stack.ts
@@ -0,0 +1,30 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import { Construct } from 'constructs';
+
+export class PulseLambdaStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: cdk.StackProps & { vpc: ec2.Vpc }) {
+    super(scope, id, props);
+
+    const healthCheckLambda = new lambda.Function(this, 'PulseHealthCheck', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async () => {
+          console.log('Health check triggered');
+        };
+      `),
+      vpc: props.vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+    });
+
+    const rule = new events.Rule(this, 'PulseHealthCheckSchedule', {
+      schedule: events.Schedule.rate(cdk.Duration.minutes(1)),
+    });
+
+    rule.addTarget(new targets.LambdaFunction(healthCheckLambda));
+  }
+}
