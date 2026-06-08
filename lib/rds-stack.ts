cb940a2e5195a5330d8d9b2e6389c14a28809f4a
Bonolo Selowa <bonololloyd003@gmail.com>
2026-06-08 15:50:57 +0200

    Firewalled RDS

--- lib/rds-stack.ts
+++ lib/rds-stack.ts
@@ -5,9 +5,16 @@ import { Construct } from 'constructs';
 
 export class PulseRdsStack extends cdk.Stack {
   public readonly db: rds.DatabaseInstance;
+  public readonly securityGroup: ec2.SecurityGroup;
 
   constructor(scope: Construct, id: string, props: cdk.StackProps & { vpc: ec2.Vpc }) {
     super(scope, id, props);
+
+    this.securityGroup = new ec2.SecurityGroup(this, 'PulseRdsSG', {
+      vpc: props.vpc,
+      description: 'Security group for Pulse RDS',
+      allowAllOutbound: false,
+    });
 
     this.db = new rds.DatabaseInstance(this, 'PulsePostgres', {
       engine: rds.DatabaseInstanceEngine.postgres({
@@ -16,6 +23,7 @@ export class PulseRdsStack extends cdk.Stack {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
       vpc: props.vpc,
       vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+      securityGroups: [this.securityGroup],
       multiAz: false,
       allocatedStorage: 20,
       deletionProtection: false,
