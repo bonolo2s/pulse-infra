dcb8252cd5209265bec5ac7c624381124146a54b
Bonolo Selowa <bonololloyd003@gmail.com>
2026-06-08 16:38:51 +0200

    vpc multi environment configired

--- lib/vpc-stack.ts
+++ lib/vpc-stack.ts
@@ -2,14 +2,18 @@ import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
+interface PulseVpcStackProps extends cdk.StackProps {
+    environment: 'dev' | 'staging' | 'prod';
+}
+
 export class PulseVpcStack extends cdk.Stack {
   public readonly vpc: ec2.Vpc;
 
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: PulseVpcStackProps) {
     super(scope, id, props);
 
-    this.vpc = new ec2.Vpc(this, 'PulseVpc', {
-      maxAzs: 2,// i need for backup when one availability zone is down.
+    this.vpc = new ec2.Vpc(this, `PulseVpc-${props.environment}`, {
+      maxAzs: 2,
       natGateways: 0,
       subnetConfiguration: [
         {
