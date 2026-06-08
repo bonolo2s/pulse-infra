fba2b63ef224a0490345c11e2262a1ca81ba2186
Bonolo Selowa <bonololloyd003@gmail.com>
2026-06-08 16:42:11 +0200

    ElasticCache multi environment configired

--- lib/elasticache-stack.ts
+++ lib/elasticache-stack.ts
@@ -3,25 +3,30 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as elasticache from 'aws-cdk-lib/aws-elasticache';
 import { Construct } from 'constructs';
 
+interface PulseElastiCacheStackProps extends cdk.StackProps {
+    vpc: ec2.Vpc;
+    environment: 'dev' | 'staging' | 'prod';
+}
+
 export class PulseElastiCacheStack extends cdk.Stack {
   public readonly securityGroup: ec2.SecurityGroup;
   public readonly cluster: elasticache.CfnCacheCluster;
 
-  constructor(scope: Construct, id: string, props: cdk.StackProps & { vpc: ec2.Vpc }) {
+  constructor(scope: Construct, id: string, props: PulseElastiCacheStackProps) {
     super(scope, id, props);
 
     this.securityGroup = new ec2.SecurityGroup(this, 'PulseRedisSG', {
       vpc: props.vpc,
-      description: 'Security group for Pulse Redis',
+      description: `Security group for Pulse Redis (${props.environment})`,
       allowAllOutbound: false,
     });
 
     const subnetGroup = new elasticache.CfnSubnetGroup(this, 'PulseRedisSubnetGroup', {
-      description: 'Subnet group for Pulse Redis',
-      subnetIds: props.vpc.privateSubnets.map(s => s.subnetId),// Grabs the IDs of all my private subnets so ElastiCache knows exactly which ones it's allowed to live in.
+      description: `Subnet group for Pulse Redis (${props.environment})`,
+      subnetIds: props.vpc.privateSubnets.map(s => s.subnetId), // Grabs the IDs of all my private subnets so ElastiCache knows exactly which ones it's allowed to live in.
     });
 
-    this.cluster = new elasticache.CfnCacheCluster(this, 'PulseRedis', {
+    this.cluster = new elasticache.CfnCacheCluster(this, `PulseRedis-${props.environment}`, {
       cacheNodeType: 'cache.t3.micro',
       engine: 'redis',
       numCacheNodes: 1,
