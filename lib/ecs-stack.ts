4eae0ff78e6232ffd832acab56f1ac8c22d15255
Bonolo Selowa <bonololloyd003@gmail.com>
2026-09-14 12:29:48 +0200

    add ecs roles n policies to ECR

--- lib/ecs-stack.ts
+++ lib/ecs-stack.ts
@@ -3,6 +3,7 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 interface PulseEcsStackProps extends cdk.StackProps {
@@ -47,21 +48,48 @@ export class PulseEcsStack extends cdk.Stack {
         });
 
         this.cluster.addCapacity('PulseEc2Capacity', {
-            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+            instanceType: ec2.InstanceType.of(
+                ec2.InstanceClass.T3,
+                ec2.InstanceSize.MICRO
+            ),
             minCapacity: 1,
             maxCapacity: isProd ? 4 : 1,
         });
 
-        const taskDefinition = new ecs.Ec2TaskDefinition(this, 'PulseTaskDef');
+        const repository = ecr.Repository.fromRepositoryName(
+            this,
+            'PulseRepo',
+            'pulse-api'
+        );
 
-        const repository = ecr.Repository.fromRepositoryName(this, 'PulseRepo', 'pulse-api');
+        // ECS infrastructure permissions
+        const executionRole = new iam.Role(this, 'PulseEcsExecutionRole', {
+            assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+            managedPolicies: [
+                iam.ManagedPolicy.fromAwsManagedPolicyName(
+                    'service-role/AmazonECSTaskExecutionRolePolicy'
+                ),
+            ],
+        });
+
+        // Permissions available to the Pulse API container
+        const taskRole = new iam.Role(this, 'PulseEcsTaskRole', {
+            assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+        });
+
+        const taskDefinition = new ecs.Ec2TaskDefinition(this, 'PulseTaskDef', {
+            executionRole,
+            taskRole,
+        });
 
         taskDefinition.addContainer('PulseApiContainer', {
             image: ecs.ContainerImage.fromEcrRepository(repository),
             memoryLimitMiB: 512,
             cpu: 256,
             portMappings: [{ containerPort: 8080 }],
-            logging: ecs.LogDrivers.awsLogs({ streamPrefix: `pulse-api-${props.environment}` }),
+            logging: ecs.LogDrivers.awsLogs({
+                streamPrefix: `pulse-api-${props.environment}`,
+            }),
         });
 
         const service = new ecs.Ec2Service(this, 'PulseService', {
